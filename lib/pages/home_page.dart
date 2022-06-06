082f03cc6b321b0ba692bb193a922b5461cdb109
Anchal <anchal.sri1688@gmail.com>
2022-06-06 12:56:59 +0530

    GridView GridTile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -46,11 +46,34 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: CatalogModel.items?.isNotEmpty == true
-            ? ListView.builder(
-                itemCount: CatalogModel.items?.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items?[index],
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
                 ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items?[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            item?.name ?? "",
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          decoration: const BoxDecoration(
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                        child: Image.network(item?.image ?? ""),
+                        footer: Text(item?.price.toString() ?? ""),
+                      ));
+                },
+                itemCount: CatalogModel.items?.length,
               )
             : const Center(
                 child: CircularProgressIndicator(),
